3dc2613363fa2a73ceec5baf05265bd544e46db5
DragonChirag <chiragvadhavanaai@gmail.com>
2023-02-28 14:22:11 +0530

    First

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 4300
+
+const multer  = require('multer')
+const {mergePdfs}  = require('./merge')
+
+const upload = multer({ dest: 'uploads/' })
+app.use('/static', express.static('public'))
+
+// Get Request
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,"templates/index.html"))
+})
+
+// Post Request
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next) => {
+  console.log(req.files)
+  let d = await mergePdfs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+   res.redirect(`http://localhost:4300/static/${d}.pdf`)
+})
+
+// Listening Port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
